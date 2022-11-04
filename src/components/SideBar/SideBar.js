c96e7c724f0bf6b9598637bf3323c7602e049c06
Lucas Lu <lucaslu@hotmail.com>
2022-11-03 22:59:42 -0700

    add sidebar mobile version

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,16 +1,25 @@
-import React from "react";
+import "./SideBar.scss";
 
 const SideBar = ({ videos, onClick }) => {
   return (
     <aside className="sidebar">
+      <h2 className="sidebar__title">NEXT VIDEOS</h2>
+
       {videos.map((video) => (
-        <img
-          key={video.id}
-          src={video.image}
-          alt={video.title}
-          width={100}
-          onClick={(clickEvnt) => onClick(clickEvnt, video.id)}
-        />
+        <article className="sidebar__videos">
+          <img
+            className="sidebar__image"
+            key={video.id}
+            src={video.image}
+            alt={video.title}
+            width={100}
+            onClick={(clickEvnt) => onClick(clickEvnt, video.id)}
+          />
+          <div className="sidebar__container">
+            <h3 className="sidebar__name">{video.title}</h3>
+            <p className="sidebar__channel">{video.channel}</p>
+          </div>
+        </article>
       ))}
     </aside>
   );
